5d0c8b1a7876632e2809050b5960fc0411495888
fena-c <82330399+fena-c@users.noreply.github.com>
2022-11-23 23:18:46 -0300

    Add files via upload

---
+++ awesome/theme/init.lua
@@ -0,0 +1,24 @@
+
+local beautiful = require("beautiful")
+
+--local gears = require("gears")
+--local awful = require("awful")
+--local menubar = require("menubar")
+--local hotkeys_popup = require("awful.hotkeys_popup")
+--
+--local naughty = require("naughty")
+
+
+local tema = "default" 
+
+
+
+
+
+beautiful.init("/home/fer/.config/awesome/theme/" .. tema .. "/theme.lua")
+
+
+
+
+
+
